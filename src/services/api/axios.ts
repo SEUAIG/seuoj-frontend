b3956aae74710e38096fa1965c10626c4de974d5
wbxnll <3400096369@qq.com>
2025-12-27 20:46:34 +0800

    Update axios.ts

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -1,6 +1,22 @@
-import axios from "axios"
-import {ENV} from "@/config/env"
+import axios from "axios";
+import { ENV } from "@/config/env";
+import { store } from "@/app/store";
+
 export const api = axios.create({
-    baseURL:ENV.API_BASE_URL,
-    timeout:10000,
-})
+  baseURL: ENV.API_BASE_URL,
+  timeout: 10000,
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const state = store.getState();
+    const token = state.auth.jwt;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
