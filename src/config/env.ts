4808b2ad638f4f9900de74824059082191049a74
wbxnll <3400096369@qq.com>
2026-03-18 17:26:58 +0800

    修复undefined路由问题

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 // 注意这里用于读取环境变量
 // Vite 使用 import.meta.env 获取环境变量，变量名必须以 VITE_ 开头
 export const ENV = {
-  API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL || "",
 };
